b1db2f48478e6ead34a95261458381cbb719fbd7
Henry <hgrant94@gmail.com>
2020-08-03 13:32:19 -0400

    remove unneeded style

--- src/index.js
+++ src/index.js
@@ -7,8 +7,6 @@ import { createGlobalStyle } from 'styled-components'
 
 const GlobalStyle = createGlobalStyle`
   *, *:after, *:before {
-    -webkit-box-sizing: border-box;
-    -moz-box-sizing: border-box;
     box-sizing: border-box;
   }
 
